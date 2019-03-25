a9fc88286d402f1cacab5a7e1110e2d39d8b7e0c
meowoodie <woodielove@163.com>
2019-03-25 13:47:46 -0400

    add pretrained gaussian mixture test

--- test.py
+++ test.py
@@ -8,17 +8,20 @@ from stppg import HawkesLam, SpatialTemporalPointProcess, StdDiffusionKernel, Ga
 from utils import plot_spatio_temporal_points, plot_spatial_intensity, plot_spatial_kernel
 
 def test_std_diffusion():
-    '''Test Spatio-Temporal Point Process Generator equipped with standard diffusion kernel'''
+    '''
+    Test Spatio-Temporal Point Process Generator equipped with 
+    standard diffusion kernel
+    '''
     # parameters initialization
     mu     = .1
-    kernel = StdDiffusionKernel(C=1., beta=1., sigma_x=.2, sigma_y=.2)
+    kernel = StdDiffusionKernel(C=1., beta=1., sigma_x=.1, sigma_y=.1)
     lam    = HawkesLam(mu, kernel, maximum=1e+3)
     pp     = SpatialTemporalPointProcess(lam)
 
     # generate points
     points, sizes = pp.generate(
         T=[0., 10.], S=[[-1., 1.], [-1., 1.]], 
-        batch_size=2, verbose=True)
+        batch_size=100, verbose=True)
     print(points)
     print(sizes)
 
@@ -31,7 +34,10 @@ def test_std_diffusion():
         t_slots=1000, grid_size=50, interval=50)
 
 def test_gaussian_diffusion():
-    '''Test Spatio-Temporal Point Process Generator equipped with Gaussian diffusion kernel'''
+    '''
+    Test Spatio-Temporal Point Process Generator equipped with 
+    Gaussian diffusion kernel
+    '''
     mu     = .1
     kernel = GaussianDiffusionKernel(
         layers=[5, 5], C=1., beta=1., 
@@ -59,17 +65,17 @@ def test_gaussian_diffusion():
     plot_spatial_intensity(lam, points[0], S=[[0., 10.], [-1., 1.], [-1., 1.]],
         t_slots=1000, grid_size=50, interval=50)
 
-def test_gaussian_mixture_diffusion():
-    '''Test Spatio-Temporal Point Process Generator equipped with Gaussian mixture diffusion kernel'''
+def test_random_gaussian_mixture_diffusion():
+    '''
+    Test Spatio-Temporal Point Process Generator equipped with 
+    random Gaussian mixture diffusion kernel
+    '''
     mu     = .2
     kernel = GaussianMixtureDiffusionKernel(
         n_comp=5, layers=[5, 5], C=1., beta=1., 
         SIGMA_SHIFT=.2, SIGMA_SCALE=.05, MU_SCALE=.05)
     lam    = HawkesLam(mu, kernel, maximum=1e+3)
     pp     = SpatialTemporalPointProcess(lam)
-
-    # # plot kernel parameters over the spatial region.
-    # plot_spatial_kernel("results/kernel.pdf", kernel, S=[[-1., 1.], [-1., 1.]], grid_size=50)
 
     # generate points
     points, sizes = pp.generate(
@@ -86,23 +92,63 @@ def test_gaussian_mixture_diffusion():
     plot_spatial_intensity(lam, points[0], S=[[0., 10.], [-1., 1.], [-1., 1.]],
         t_slots=1000, grid_size=50, interval=50)
 
+def test_pretrain_gaussian_mixture_diffusion():
+    '''
+    Test Spatio-Temporal Point Process Generator equipped with 
+    pretrained Gaussian mixture diffusion kernel
+    '''
+    params = np.load('data/gaussian_mixture_params.npy')
+    mu     = params['mu']
+    kernel = GaussianMixtureDiffusionKernel(
+        n_comp=5, layers=[5], C=1., beta=params['beta'], 
+        SIGMA_SHIFT=.05, SIGMA_SCALE=.1, MU_SCALE=.05,
+        Wss=params['Wss'], bss=params['bss'], Wphis=params['Wphis'])
+    lam    = HawkesLam(mu, kernel, maximum=1e+3)
+    pp     = SpatialTemporalPointProcess(lam)
+
+    # # generate points
+    # points, sizes = pp.generate(
+    #     T=[0., 10.], S=[[-1., 1.], [-1., 1.]], 
+    #     batch_size=2, verbose=True)
+    # print(points.shape)
+    # print(sizes)
+
+    # read or save to local npy file.
+    points = np.load('data/apd.crime.perday.npy')
+    # np.save('results/gaussian_hpp_Mar_15_layer_5.npy', points)
+
+    # plot intensity of the process over the time
+    plot_spatial_intensity(lam, points[0], S=[[0., 10.], [-1., 1.], [-1., 1.]],
+        t_slots=1000, grid_size=50, interval=50)
+
 
 if __name__ == '__main__':
-    np.random.seed(11)
+    np.random.seed(1)
     np.set_printoptions(suppress=True)
 
-    test_std_diffusion()
+    test_pretrain_gaussian_mixture_diffusion()
+
+    # T = [0., 10.]
+    # S = [[-1., 1.], [-1., 1.]]
 
     # mu     = .1
-    # kernel = GaussianDiffusionKernel(
-    #     layers=[5, 5], C=1., beta=1., 
-    #     SIGMA_SHIFT=.2, SIGMA_SCALE=.05, MU_SCALE=.1, is_centered=True)
+    # # kernel = StdDiffusionKernel(C=1., beta=1., sigma_x=.08, sigma_y=.08)
+    # kernel = GaussianMixtureDiffusionKernel(
+    #     n_comp=5, layers=[5], 
+    #     beta=1., C=1., SIGMA_SHIFT=.1, SIGMA_SCALE=.15, MU_SCALE=.1,
+    #     Wss=None, bss=None, Wphis=None)
     # lam    = HawkesLam(mu, kernel, maximum=1e+3)
     # pp     = SpatialTemporalPointProcess(lam)
 
-    # plot_spatial_kernel("results/kernel.pdf", kernel, S=[[-1., 1.], [-1., 1.]], grid_size=50)
+    # points = np.load('data/apd.crime.perday.npy')
+    # print(len(np.nonzero(points[0, :, 0])[0]))
+    # print(points[0])
 
-    # points = np.load('results/hpp_Feb_25.npy')
-    # print(points)
     # plot_spatial_intensity(lam, points[0], S=[[0., 10.], [-1., 1.], [-1., 1.]],
-    #     t_slots=1000, grid_size=50, interval=50)
+    #     t_slots=1000, grid_size=50, interval=100)
+
+    # # t = np.array([10])
+    # # s = np.array([1,1])
+    # # his_t = np.array([1, 2, 3])
+    # # his_s = np.array([[.1, .2] ,[.2, .3], [.3, .4]])
+    # # kernel.nu(t, s, his_t, his_s)
